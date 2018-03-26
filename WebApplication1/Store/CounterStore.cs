875f80482fef0eaa8378db121a10822e828fbf19
Robert Clarke <Robert.Clarke@gocompare.com>
2018-03-26 12:54:29 +0100

    some adjustments

--- WebApplication1/Store/CounterStore.cs
+++ WebApplication1/Store/CounterStore.cs
@@ -7,102 +7,116 @@ namespace WebApplication1.Store
 {
     public class CounterStore : IStore
     {
-        //Model
-        private bool initialized = false;
+        const int MS_MODIFIER = 50;
 
-        public double CurrentCount { get; private set; } = 0;
-        public double CountPerSecond { get; private set; } = 0;
-        public List<Building> Buildings { get; private set; } = new List<Building>()
+        //CTOR  
+        public CounterStore()
         {
-        new Building(1, 1, "Building One", 0, 10),
-        new Building(2, 10, "Building Two", 0, 50)
-        };
-        public int MsModifier { get; private set; } = 100;
-
-        public event EventHandler StateHasChanged;
-
-        public void Initialize()
-        {
-            if (!initialized)
-            {
-                TimeSpan startTimeSpan = TimeSpan.Zero;
-                TimeSpan periodTimeSpan = TimeSpan.FromMilliseconds(MsModifier);
-
-                System.Threading.Timer timer = new System.Threading.Timer((e) =>
+            this.Counter = new Counter(100, 0, new List<Building>()
                 {
-                    this.Update();
-                }, null, startTimeSpan, periodTimeSpan);
-                initialized = true;
-            }
+                    new Building(1, 1, "Building One", 0, 10),
+                    new Building(2, 10, "Building Two", 0, 50)
+                });
+            TimeSpan startTimeSpan = TimeSpan.Zero;
+            TimeSpan periodTimeSpan = TimeSpan.FromMilliseconds(MS_MODIFIER);
+
+            System.Threading.Timer timer = new System.Threading.Timer((e) =>
+            {
+                this.Update();
+            }, null, startTimeSpan, periodTimeSpan);
         }
+
+        //MODEL
+        public event EventHandler StateHasChanged;
+        public Counter Counter { get; private set; }
 
         //UPDATE
         public int GetCount()
         {
-            return (int)Math.Round(this.CurrentCount);
+            return (int)Math.Round(this.Counter.CurrentCount);
         }
 
         public void IncrementCount()
         {
-            this.CurrentCount++;
+            this.Counter.CurrentCount++;
         }
 
         public void Update()
         {
+            CalculateCountPerSecond();
+            this.Counter.CurrentCount += Counter.CountPerSecond / (1000 / MS_MODIFIER);
+            StateChanged(new EventArgs());
+        }
+
+        public void CalculateCountPerSecond()
+        {
             double amountPerSecond = 0;
-            foreach (var building in this.Buildings)
+            foreach (var building in this.Counter.Buildings)
             {
                 amountPerSecond += building.CalculateClicksPerSecond();
             }
-            this.CountPerSecond = amountPerSecond;
-            this.CurrentCount += CountPerSecond / (1000 / this.MsModifier);
-            StateChanged(new EventArgs());
+            this.Counter.CountPerSecond = amountPerSecond;
         }
 
         public void BuyBuilding(int buildingId)
         {
-            var building = Buildings.FirstOrDefault(e => e.Id == buildingId);
-            if (building.Cost <= this.CurrentCount)
+            var building = Counter.Buildings.FirstOrDefault(e => e.Id == buildingId);
+            if (building.Cost <= this.Counter.CurrentCount)
             {
-                building.BuyOne();
-                this.CurrentCount -= building.Cost;
+                building.AmountOwned++;
+                this.Counter.CurrentCount -= building.Cost;
+                CalculateCountPerSecond();
             }
         }
         protected virtual void StateChanged(EventArgs args)
         {
             StateHasChanged?.Invoke(this, args);
+        }        
+    }
+
+    //MODELS
+    public class Counter
+    {
+        //CTOR
+        public Counter(int currentCount, int countPerSecond, List<Building> buildings)
+        {
+            this.CurrentCount = currentCount;
+            this.CountPerSecond = countPerSecond;
+            this.Buildings = buildings;
         }
 
-        //OTHER MODELS
-        public class Building
+        //MODEL
+        public double CurrentCount { get; internal set; } = 0;
+        public double CountPerSecond { get; internal set; } = 0;
+        public List<Building> Buildings { get; internal set; } = new List<Building>()
         {
-            //CONSTRUCTOR
-            public Building(int id, int amountPerSecond, string name, int amountOwned, int cost)
-            {
-                this.Id = id;
-                this.AmountPerSecond = amountPerSecond;
-                this.Name = name;
-                this.AmountOwned = AmountOwned;
-                this.Cost = cost;
-            }
+            new Building(1, 1, "Building One", 0, 10),
+            new Building(2, 10, "Building Two", 0, 50)
+        };
+    }
 
-            //MODEL
-            public int Id { get; private set; }
-            public int AmountPerSecond { get; private set; }
-            public string Name { get; private set; }
-            public int AmountOwned { get; private set; }
-            public int Cost { get; set; }
+    public class Building
+    {
+        //CONSTRUCTOR
+        public Building(int id, int amountPerSecond, string name, int amountOwned, int cost)
+        {
+            this.Id = id;
+            this.AmountPerSecond = amountPerSecond;
+            this.Name = name;
+            this.AmountOwned = amountOwned;
+            this.Cost = cost;
+        }
 
-            //UPDATE
-            public void BuyOne()
-            {
-                this.AmountOwned++;
-            }
+        //MODEL
+        public int Id { get; internal set; }
+        public int AmountPerSecond { get; internal set; }
+        public string Name { get; internal set; }
+        public int AmountOwned { get; internal set; }
+        public int Cost { get; internal set; }
 
-            public double CalculateClicksPerSecond()
-            {
-                return this.AmountOwned * this.AmountPerSecond;
-            }
+        public double CalculateClicksPerSecond()
+        {
+            return this.AmountOwned * this.AmountPerSecond;
         }
     }
 }
